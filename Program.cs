7970e5bc6bdc819250f8e0ceee1810b28528a604
gaston hernan rosales <herlivv578@gmail.com>
2025-10-29 17:40:16 -0300

    Fix CORS for backend-only deploy

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("https://tu-frontend.onrender.com") 
+            policy.WithOrigins() 
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
