e69568784e01c0ab7ae14ead70089a75807b7953
gaston hernan rosales <herlivv578@gmail.com>
2025-10-29 21:54:26 -0300

    Fix: CORS para frontend de Render

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.AllowAnyOrigin()
+            policy.WithOrigins("https://mi-frontend-gjwc.onrender.com")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
